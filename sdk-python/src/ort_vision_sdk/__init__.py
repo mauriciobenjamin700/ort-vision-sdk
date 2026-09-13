2da3c5f030cf3132b631a012b47fc09010af973e
mauricio <mauriciobenjamin700@gmail.com>
2026-09-13 15:32:14 -0300

    chore(python): release v0.11.0

--- sdk-python/src/ort_vision_sdk/__init__.py
+++ sdk-python/src/ort_vision_sdk/__init__.py
@@ -112,7 +112,7 @@ from ort_vision_sdk.types import DetectionResult as DetectionResult
 from ort_vision_sdk.types import ImageArray as ImageArray
 from ort_vision_sdk.types import SegmentationResult as SegmentationResult
 
-__version__: str = "0.10.0"
+__version__: str = "0.11.0"
 
 __all__: list[str] = [
     "COCO_CLASSES",
